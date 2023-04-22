a08c9f96a16d69783b7fe214676ccc1e626ee9c8
Elena <katayeva08@gmail.com>
2023-04-22 12:00:32 +0300

    add classes in main.js

--- main.js
+++ main.js
@@ -322,3 +322,54 @@ function getTotalProgressByRecursion(data) {
 const result = getTotalProgressByRecursion(students);
 console.log(result[0]/result[1]);
 // рекурсия работает с любой вложенностью, т.е в объект можно сколько угодно добавлять новых объектов, массивов
+
+
+// homework recursion task
+
+function factorial(n) {
+  if (typeof(n) !== 'number' || !Number.isInteger(n)) {
+    return 'Error';
+  }
+  if(n >= 1) {
+    return n * factorial(n -1);
+  } else {
+    return 1;
+  }
+  //  return n ? n * factorial(n - 1) : 1;
+}
+console.log(factorial(9));
+
+
+// classes 
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width); 
+    this.text = text;
+    this.bgColor = bgColor;
+  }
+
+  showMyProps() {
+    console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+  }
+}
+
+const div = new ColoredRectangleWithText(25, 40, 'Hello', 'purple');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 20);
+// const long = new Rectangle(50, 40);
+// console.log(square.calcArea());
+// console.log(long.calcArea());
